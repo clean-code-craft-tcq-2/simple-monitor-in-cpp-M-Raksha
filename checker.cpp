3d09453bb2da52145a8345fe523da5aa104c4c58
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-02-08 20:39:38 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,19 +2,50 @@
 #include <iostream>
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
+#define parameterstocheck 3
+
+float upperlimits[parameterstocheck] = {45,80,0.8};
+float lowerLimits[parameterstocheck] ={0,20,0};
+float CurrentVal[parameterstocheck];
+std::string warnings[parameterstocheck] = {"Temperature out of range!", "State of Charge out of range!","Charge Rate out of range!"};
+
+enum batteryParameter
+{
+    TEMP,
+    SOC1,
+    CHARGERATE
+};
+
+bool isInRange(float CurrentVal, float UpperLimit, float LowerLimit)
+{
+    return (CurrentVal>=LowerLimit && CurrentVal<= UpperLimit)? true:false;
 }
+ 
+void assignCurrValue(int a, int b, int c)
+{
+    CurrentVal[TEMP] = a;
+    CurrentVal[SOC1] = b;
+    CurrentVal[CHARGERATE] =c;
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate)
+{
+    int retValue; 
+    int i=0;
+   assignCurrValue(temperature,soc,chargeRate);
+   
+   while(retValue != false && i < parameterstocheck)
+   {
+       retValue = isInRange(CurrentVal[i],upperlimits[i],lowerLimits[i]);
+       i++;
+   }
+   if(retValue != true)
+	cout<<warnings[i-1]<<"\n";
+	
+  return retValue;
+}
+
+
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
